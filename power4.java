56cf76fcf9125ecb6e5be0314ccd4b63adae5e4d
Raayavegas <veeralarithika@gmail.com>
2024-12-04 13:57:36 +0530

    Add files via upload

---
+++ power4.java
@@ -0,0 +1,14 @@
+class A 
+{
+    public static void main(string args[])
+    {
+        int a=56;
+        if((a&(a-2)==0))
+        {
+            System.out.println("yes");
+        }
+        else{
+            System.out.println("no");
+        }
+    }
+}
