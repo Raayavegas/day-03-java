813675f7b4c3acea5e5eea16533a52fb7d964991
Raayavegas <veeralarithika@gmail.com>
2024-12-04 11:01:29 +0530

    Add files via upload

---
+++ setbit.java
@@ -0,0 +1,16 @@
+class A 
+{
+    public static void main(String args[])
+    {
+        int a=10;
+        int pos=1;
+        if((a&(1<<pos))!=0)
+        {
+            System.out.println("setbit");
+        }
+        else
+        {
+            System.out.println("not setbit");
+        }
+    }
+}
