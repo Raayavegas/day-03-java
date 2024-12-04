f43f54889e180300fbd7ef2132590befb1786178
Raayavegas <veeralarithika@gmail.com>
2024-12-04 14:07:22 +0530

    Add files via upload

---
+++ multiple.java
@@ -0,0 +1,14 @@
+class A 
+{
+    public static void main(String args[])
+    {
+        int n=20;
+        if((n&3)==0)
+        {
+            System.out.println("multiple");
+        }
+        else{
+            System.out.println("not multiple");
+        }
+    }
+}
