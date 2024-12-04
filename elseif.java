ae4bda60075871af907ae660990977b41d68d4a8
Raayavegas <veeralarithika@gmail.com>
2024-12-04 15:13:38 +0530

    Add files via upload

---
+++ elseif.java
@@ -0,0 +1,18 @@
+class A 
+{
+    public static void main(String args[])
+    {
+        int a=10,b=20,c=5;
+        if((a>b)&&(a>c))
+        {
+            System.out.println("a is large");
+        }
+        else if((b>a)&&(b>c))
+        {
+            System.out.println(" bis largest");
+        }
+        else{
+            System.out.println(" c is largest");
+        }
+    }
+}
